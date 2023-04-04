13a8417b3e7eec18f1e2647fe974d573c43e53d5
Aaryan Agrawal <126146808+Aaryan-Agr@users.noreply.github.com>
2023-04-03 23:51:05 -0400

    Add files via upload

---
+++ TwoNumbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+		Descrip[tion:
+		This functions finds two unique index numbers that when added up gives the value the same as target.
+		
+		Parameters:
+		Nums - List of int
+		target - int
+
+		Returns:
+		arr - List of all possible indices that match the target (Stored as pairs)
+ 
+		
+        '''
+        arr = []
+	  
+	  #Loop to iterate for the first index
+        for i in range(len(nums)):
+		#Loop to iterate for the second index
+            for j in range(len(nums)):
+		    #Checking for conditions :- values at index i,j give the same value;i and j are not already in 'arr'; i and j are not the same index
+                if nums[i]+nums[j] == target and ((i not in arr)and (j not in arr)) and i!=j:
+                    arr.append(i)
+                    arr.append(j)
+
+        return arr
